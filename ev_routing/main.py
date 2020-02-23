abf0ecbbad67633bfc7700f91309f6f7c3ba0c28
nilo0 <nil.rahmati@gmail.com>
2020-02-23 14:20:43 +0100

    wip : f_w profile -- implimented link operator

--- ev_routing/main.py
+++ ev_routing/main.py
@@ -90,6 +90,95 @@ class EVRouting:
             return default_SoC(), SoC, []
 
 
+
+
+
+    def fw_profile(self, stations, M):
+        """
+        Args:
+        stations: list of charging stations ids, e.g. [1, 203, 453]
+        M: Battery charge capacity
+        """
+        l = self._soc_initialise(stations, M)
+
+        # New set of break points after linking two paths
+        l_new = []
+
+        def f(break_points, ic):
+            """
+            Args:
+            break_points: List of break points and their final charges
+            ic: Inital charge
+            """
+            I = [bp['point'][0] for bp in break_points] # Initial charge
+            F = [bp['point'][1] for bp in break_points] # Final charge
+            S = [bp['slope'] for bp in break_points] # Right-side slope
+
+            for i in range(len(I) - 1):
+                if I[i] <= ic < I[i+1]:
+                    if S[i] == 0:
+                        return F[i]
+                    else:
+                        return ic - I[i] + F[i]
+
+
+        n = len(stations)
+
+        for k in range(n):
+            for i in range(n):
+                l_ik = l[i][k]
+
+                ik_maps = []
+                for ik in l_ik:
+                    ik_maps.append({
+                        'domain': (ik['point'][0], ik['point'][0] + ik['xlength']),
+                        'range': (ik['point'][1], ik['point'][1] + ik['slope'] * ik['xlength']),
+                    })
+
+                for j in range(n):
+                    l_kj = l[k][j]
+
+                    for ik in l_ik:
+                        l_new.append(
+                            self._soc_segment((ik['point'][0], f(l_kj, ik['point'][1])), None, None)
+                        )
+
+                    for kj in l_kj:
+                        for ik_map in ik_maps:
+                            if ik_map['range'][0] <= kj['point'][0] < ik_map['range'][1]:
+                                l_new.append(
+                                    self._soc_segment((ik_map['domain'][0] , kj['point'][1]), None, None)
+                                )
+                                break
+
+                    # Removing duplicated element from l_new
+                    l_new = self._soc_remove_unnecessary_break_points(l_new)
+
+
+    def _soc_remove_unnecessary_break_points(self, l):
+        """
+        Removeing break points with the same x but different ys
+
+        Args:
+        l: List of break points after linking
+        """
+        l_ikj = [l[0]]
+
+        for bp in l[1:]:
+            found = False
+
+            for i in range(len(l_ikj)):
+                ikj = l_ikj[i]
+                if ikj['point'][0] == bp['point'][0]:
+                    found = True
+                    if bp['point'][1] > ikj['point'][1]:
+                        l_ikj[i] = bp
+            if not found:
+                l_ikj.append(bp)
+
+        return l_ikj
+
+
     def _soc_initialise(self, stations, M):
         """
         Args:
@@ -97,18 +186,27 @@ class EVRouting:
         M: Battery charge capacity
         """
         n = len(stations)
-        l = [[[]]*n]*n
+        l = []
 
         for i in range(n):
+            row = []
             for j in range(n):
-                e = self.map.is_connected(stations[i], stations[j])
-                if e:
-                    l[i][j] = self._set_of_break_points(e, M)
+                if i == j:
+                    row.append([
+                        self._soc_segment((0, 0), 1, 0),
+                        self._soc_segment((M, M), 0, 0),
+                    ])
                 else:
-                    l[i][j] = [(0, float('-inf')), (M, float('-inf'))]
+                    e = self.map.is_connected(stations[i], stations[j])
+                    if e:
+                        row.append(self._set_of_break_points(e, M))
+                    else:
+                        row.append([
+                            self._soc_segment((0, float('-inf')), 0, 0),
+                            self._soc_segment((M, float('-inf')), 0, 0),
+                        ])
 
-        for i in range(n):
-            l[i][i] = [(0, 0), (M, M)]
+            l.append(row)
 
         return l
 
@@ -126,6 +224,26 @@ class EVRouting:
         c = e['cost']
 
         if c < 0:
-            return [(0, c), (M + c, M), (M, M)]
+            return [
+                self._soc_segment((0, -c), 1, M + c),
+                self._soc_segment((M + c, M), 0, -c),
+                self._soc_segment((M, M), 0, 0),
+            ]
         else:
-            return [(c, 0), (M, M - c )]
+            return [
+                self._soc_segment((c, 0), 1, M-c),
+                self._soc_segment((M, M - c), 1, 0),
+            ]
+
+    def _soc_segment(self, point, slope, xlength):
+        """
+        Args:
+        point: Starting point of the segment
+        slope: Slope of the segment
+        xlength: Projected length of the segment on x-axis (initial charge axis)
+        """
+        return {
+            'point': point,
+            'slope': slope,
+            'xlength': xlength,
+        }
